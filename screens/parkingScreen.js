50f48fe61a6a02ddd658985148b24ddd8d174348
IvyMasterDiego <diegolobato82@gmail.com>
2023-09-24 18:42:06 -0400

    Creacion de la vista de parqueo

--- screens/parkingScreen.js
+++ screens/parkingScreen.js
@@ -4,16 +4,54 @@ import { TouchableOpacity } from "react-native-gesture-handler";
 import { Table, Row, Rows } from "react-native-table-component";
 
 export default function ParkingScreen({ navigation }) {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Parking Screeen</Text>
-            <Button onPress={() => navigation.navigate("Debug")}>
-                Debug
-            </Button>
-        </View>
-    );
+  const tableData = [
+    ["Estacionamiento", "Parqueo", "Precio"],
+    ["Parqueo Subterraneo", "1S", "150"],
+  ];
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Parking Screeen</Text>
+      <Table borderStyle={{ borderWidth: 1 }}>
+        <Row
+          data={["Estacionamiento", "Parqueo", "Precio"]}
+          style={styles.head}
+          textStyle={[styles.text, { color: "white" }]}
+        />
+        <Rows data={tableData.slice(1)} textStyle={styles.text} />
+      </Table>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          style={[styles.button, { backgroundColor: "#6563db" }]}
+          onPress={() => navigation.navigate("CreateParking")}>
+          <Text style={styles.buttonText}>Add Parking</Text>
+        </TouchableOpacity>
+        <Button onPress={() => navigation.navigate("Debug")}>Debug</Button>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    title: { fontSize: 24, marginBottom: 20, textAlign: "center" },
+  container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: "#fff" },
+  title: { fontSize: 24, marginBottom: 20, textAlign: "center" },
+  head: { height: 40, backgroundColor: "#6563db" },
+  text: { margin: 6, textAlign: "center" },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 20,
+  },
+  button: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+    paddingVertical: 10,
+    fullWidth: true,
+    width: "100%",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 18,
+  },
 });

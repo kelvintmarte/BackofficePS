8654253d3538981f675558a193eaac9fd095fe3b
IvyMasterDiego <diegolobato82@gmail.com>
2023-09-24 18:43:02 -0400

    crear parqueo

--- screens/createScreens/createParking.js
+++ screens/createScreens/createParking.js
@@ -1,19 +1,168 @@
-import React, { Component } from "react";
-import { Text, View, StyleSheet, Button } from "react-native";
+import {React,  Component,useState } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Button,
+  TextInput,
+  Modal,
+  TouchableWithoutFeedback,
+} from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
-import { Table, Row, Rows } from "react-native-table-component";
+// import { Table, Row, Rows } from "react-native-table-component
 
 export default function CreateParkingScreen({ navigation }) {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Create Parking Screeen</Text>
-            <Button onPress={() => navigation.navigate("Debug")}>
-                Debug
-            </Button>
+  const [isModalVisible, setModalVisible] = useState(false);
+  const [selectedValue, setSelectedValue] = useState(null);
+  const [textInput1, setTextInput1] = useState("");
+  const [textInput2, setTextInput2] = useState("");
+
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
+
+  const handleOptionSelect = (value) => {
+    setSelectedValue(value);
+    toggleModal();
+  };
+
+  // Data for the combo box (dropdown)
+  const comboData = [
+    { label: "Parqueo Subterraneo", value: "Parqueo Subterraneo" },
+    { label: "Parqueo Profesores", value: "Parqueo Profesores" },
+    { label: "Parqueo Biblioteca", value: "Parqueo Biblioteca" },
+  ];
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Add Parking</Text>
+      <Text style={styles.label}>Select an option:</Text>
+      <TouchableOpacity onPress={toggleModal} style={styles.comboButton}>
+        <Text>{selectedValue ? selectedValue : "Select an option..."}</Text>
+      </TouchableOpacity>
+
+      <Text style={styles.label}>Parking:</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={(text) => setTextInput1(text)}
+        value={textInput1}
+      />
+
+      <Text style={styles.label}>Base Price:</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={(text) => setTextInput2(text)}
+        value={textInput2}
+      />
+
+      <Modal animationType="slide" transparent={true} visible={isModalVisible}>
+        <TouchableWithoutFeedback onPress={toggleModal}>
+          <View style={styles.modalOverlay}></View>
+        </TouchableWithoutFeedback>
+        <View style={styles.modalContent}>
+          <Text style={styles.modalTitle}>Select an option:</Text>
+          {comboData.map((item) => (
+            <TouchableOpacity
+              key={item.value}
+              style={styles.modalOption}
+              onPress={() => handleOptionSelect(item.value)}>
+              <Text>{item.label}</Text>
+            </TouchableOpacity>
+          ))}
         </View>
-    );
+      </Modal>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          style={[styles.button, { backgroundColor: "#6563db" }]}
+          onPress={() => navigation.navigate("Parking")}>
+          <Text style={styles.buttonText}>Back</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.button, { backgroundColor: "#6563db" }]}>
+          <Text style={styles.buttonText}>Add Parking</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    title: { fontSize: 24, marginBottom: 20, textAlign: "center" },
+  title: { fontSize: 24, marginBottom: 20, textAlign: "center" },
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "#fff",
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 4,
+  },
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 16,
+    paddingLeft: 8,
+  },
+  comboButton: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 16,
+    justifyContent: "center",
+    paddingLeft: 8,
+  },
+  button: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+    paddingVertical: 10,
+    fullWidth: true,
+    width: "100%",
+  },
+  modalOverlay: {
+    flex: 1,
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  modalContent: {
+    backgroundColor: "white",
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+  modalTitle: {
+    fontSize: 18,
+    padding: 16,
+    borderBottomColor: "gray",
+    borderBottomWidth: 1,
+  },
+  modalOption: {
+    padding: 16,
+    borderBottomColor: "gray",
+    borderBottomWidth: 1,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 18,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 20,
+  },
+  button: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+    paddingVertical: 10,
+    fullWidth: true,
+    width: "100%",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 18,
+  },
 });

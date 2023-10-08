3b15cce25523ba71e86fa4b68c64158e357ed39e
kelvintmarte <kelvintmarte@gmail.com>
2023-10-08 18:53:49 -0400

    cambios

--- app/index.js
+++ app/index.js
@@ -3,7 +3,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useRouter } from 'expo-router';
 import LoginScreen from '../screens/loginScreen.js';
-import RegisterScreen from '../screens/registerScreen.js';
 import StartingScreen from '../screens/startingScreen.js';
 import ParkingLotScreen from '../screens/parkinglotScreen.js';
 import DebugScreen from '../screens/debugScreen.js';
@@ -13,8 +12,6 @@ import CreateOrganizationScreen from '../screens/createScreens/createOrganizatio
 import CreateParkingScreen from '../screens/createScreens/createParking.js';
 import CreateParkingLotScreen from '../screens/createScreens/createParkingLot.js';
 import ConfigScreen from '../screens/mainScreens/configScreen.js';
-import ProfileScreen from '../screens/profileScreen.js';
-import EditProfileScreen from '../screens/editProfileScreen.js';
 import DocumentationScreen from '../screens/mainScreens/documentationScreen.js';
 import supportScreen from '../screens/supportScreen.js';
 import DashboardScreen from '../screens/mainScreens/dashboardScreen.js';
@@ -27,17 +24,14 @@ const App = () => {
             <Stack.Navigator screenOptions={{ headerShown: false }}>
                 <Stack.Screen name="Starting" component={StartingScreen} />
                 <Stack.Screen name="Login" component={LoginScreen} />
-                <Stack.Screen name="Register" component={RegisterScreen} />
-                <Stack.Screen name="ParkingLot" component={ParkingLotScreen} />
                 <Stack.Screen name="Debug" component={DebugScreen} />
                 <Stack.Screen name="Parking" component={ParkingScreen} />
+                <Stack.Screen name="ParkingLot" component={ParkingLotScreen} />
                 <Stack.Screen name="Organization" component={OrganizationScreen} />
                 <Stack.Screen name="CreateOrganization" component={CreateOrganizationScreen} />
                 <Stack.Screen name="CreateParking" component={CreateParkingScreen} />
                 <Stack.Screen name="CreateParkingLot" component={CreateParkingLotScreen} />
                 <Stack.Screen name="Configuration" component={ConfigScreen} />
-                <Stack.Screen name="Profile" component={ProfileScreen} />
-                <Stack.Screen name="EditProfile" component={EditProfileScreen} />
                 <Stack.Screen name="Documentation" component={DocumentationScreen} />
                 <Stack.Screen name="Support" component={supportScreen} />
                 <Stack.Screen name="Dashboard" component={DashboardScreen} />

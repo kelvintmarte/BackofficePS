f76c3e4ec948d518a43ed4a69fda692ae5c6b688
dlobato <dlobato@ofimatic.com>
2023-10-12 16:48:57 -0400

    fix profile screen

--- screens/mainScreens/profileScreen.js
+++ screens/mainScreens/profileScreen.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { View, Text, Image, StyleSheet } from 'react-native';
 
-export default function profileScreen({ route }) {
-    const { name, lastName, profilePicture } = route.params;
+export default function profileScreen() {
+    // const { name, lastName, profilePicture } = route.params;
   
     return (
       <View style={styles.container}>
-        <Image source={{ uri: profilePicture }} style={styles.profilePicture} />
-        <Text style={styles.name}>{name}</Text>
-        <Text style={styles.lastName}>{lastName}</Text>
+        <Image source={require("../../assets/user.png")} style={styles.profilePicture} />
+        <Text style={styles.name}>name</Text>
+        <Text style={styles.lastName}>lastName</Text>
       </View>
     );
   }
